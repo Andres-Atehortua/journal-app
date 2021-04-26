49bce7fc06438c5658629ca7a5acfbe04269dd42
Andrés López Atehortua <anderson17colombia@hotmail.com>
2021-04-26 23:52:22 +0200

    TESTING notesActions

--- src/tests/redux/actions/notesActions.test.js
+++ src/tests/redux/actions/notesActions.test.js
@@ -1,8 +1,18 @@
+/**
+ * @jest-environment node
+ */
+
+// Para las variables de entorno en testing
+
 // Configuracion para testear acciones asincronas en redux
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { db } from '../../../firebase/firebaseConfig';
-import { startNewNoteAction } from '../../../redux/actions/notesActions';
+import {
+  startLoadingNotesAction,
+  startNewNoteAction,
+  startSaveNoteAction,
+} from '../../../redux/actions/notesActions';
 import { notesTypes } from '../../../redux/types/notesTypes';
 
 // Configuracion para testear acciones asincronas en redux
@@ -16,6 +26,10 @@ const store = mockStore({
 });
 
 describe('Testing notesActions', () => {
+  beforeEach(() => {
+    store.clearActions();
+  });
+
   test('should create new note', async () => {
     await store.dispatch(startNewNoteAction());
 
@@ -45,5 +59,51 @@ describe('Testing notesActions', () => {
     const { uid } = store.getState().auth;
 
     await db.doc(`${uid}/journal/notes/${docId}`).delete();
+  });
+
+  test('should load notes when call startLoadingNotesAction', async () => {
+    const { uid } = store.getState().auth;
+
+    await store.dispatch(startLoadingNotesAction(uid));
+
+    const actions = store.getActions();
+
+    const payloadExpected = {
+      id: expect.any(String),
+      body: expect.any(String),
+      title: expect.any(String),
+      date: expect.any(Number),
+    };
+
+    expect(actions[0]).toEqual({
+      type: notesTypes.NOTES_LOAD,
+      payload: expect.any(Array),
+    });
+
+    expect(actions[0].payload[0]).toMatchObject(payloadExpected);
+  });
+
+  test('startSaveNoteAction should save and upgrade a note', async () => {
+    const { uid } = store.getState().auth;
+
+    const note = {
+      id: 'IKm1XS4fHMZuMZ4H7PZ3',
+      body: 'testing',
+      title: 'testing',
+    };
+
+    await store.dispatch(startSaveNoteAction(note));
+
+    const actions = store.getActions();
+
+    expect(actions[0]).toEqual({
+      type: notesTypes.NOTES_UPDATE,
+      payload: expect.any(Object),
+    });
+
+    const docRef = await db.doc(`${uid}/journal/notes/${note.id}`).get();
+
+    expect(docRef.data().title).toBe(note.title);
+    expect(docRef.data().body).toBe(note.body);
   });
 });
